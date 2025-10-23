d163a0b8ee291c37b999228062b3389c43c746a1
bruce-batman <khan.aujla2@gmail.com>
2025-10-23 11:30:10 +0500

    Update storage.js

--- utils/storage.js
+++ utils/storage.js
@@ -8,42 +8,42 @@ const messagesCol = 'messagess';
 let client;
 
 async function connect() {
-    if (!client) {
-        client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
-        await client.connect();
-    }
-    return client.db(dbName);
+  if (!client) {
+    client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await client.connect();
+  }
+  return client.db(dbName);
 }
 
 async function findUserByLinkId(linkId) {
-    const db = await connect();
-    return db.collection(usersCol).findOne({ linkId });
+  const db = await connect();
+  return db.collection(usersCol).findOne({ linkId });
 }
 
 async function findUserByUsername(username) {
-    const db = await connect();
-    return db.collection(usersCol).findOne({ username });
+  const db = await connect();
+  return db.collection(usersCol).findOne({ username });
 }
 
 async function addUser(user) {
-    const db = await connect();
-    await db.collection(usersCol).insertOne(user);
+  const db = await connect();
+  await db.collection(usersCol).insertOne(user);
 }
 
 async function getMessages(linkId) {
-    const db = await connect();
-    return db.collection(messagesCol).find({ linkId }).sort({ createdAt: -1 }).toArray();
+  const db = await connect();
+  return db.collection(messagesCol).find({ linkId }).toArray();
 }
 
 async function addMessage(message) {
-    const db = await connect();
-    await db.collection(messagesCol).insertOne(message);
+  const db = await connect();
+  await db.collection(messagesCol).insertOne(message);
 }
 
 async function deleteMessage(linkId, messageId) {
-    const db = await connect();
-    const result = await db.collection(messagesCol).deleteOne({ linkId, messageId });
-    return result.deletedCount === 1;
+  const db = await connect();
+  const result = await db.collection(messagesCol).deleteOne({ linkId, messageId });
+  return result.deletedCount === 1;
 }
 
 module.exports = {
