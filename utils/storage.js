921feff78236355f5eb7630981479afc275f4609
bruce-batman <khan.aujla2@gmail.com>
2025-10-23 11:14:55 +0500

    Update storage.js

--- utils/storage.js
+++ utils/storage.js
@@ -1,65 +1,56 @@
-const BIN_ID = '68f980cbae596e708f2537fa';
-const JSONBIN_KEY = '$2a$10$aVJi0c6HQUMfEbmKHifNXuz25P7oZXqrlHTNZsAZXJ./DrNPIgE3y';
+const { MongoClient } = require('mongodb');
 
-const BASE_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
-const READ_URL = `${BASE_URL}/latest`;
+const uri = process.env.MONGODB_URI || 'mongodb+srv://makki971:makki8971@cluster0.n8lg0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0';
+const dbName = 'secretmsgg';
+const usersCol = 'userss';
+const messagesCol = 'messagess';
 
-async function loadData() {
-  try {
-    const res = await fetch(READ_URL, {
-      headers: { 'X-Master-Key': JSONBIN_KEY }
-    });
-    if (!res.ok) throw new Error(`Failed to load bin (${res.status})`);
-    const json = await res.json();
-    return json.record || json;
-  } catch (err) {
-    console.error('Error loading JSONBin:', err.message);
-    return { users: [], messages: {} };
-  }
-}
+let client;
 
-async function saveData(data) {
-  const res = await fetch(BASE_URL, {
-    method: 'PUT',
-    headers: {
-      'Content-Type': 'application/json',
-      'X-Master-Key': JSONBIN_KEY
-    },
-    body: JSON.stringify(data)
-  });
-  if (!res.ok) throw new Error(`Save failed: ${res.status}`);
-  return true;
+async function connect() {
+    if (!client) {
+        client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+    }
+    return client.db(dbName);
 }
 
 async function findUserByLinkId(linkId) {
-  const data = await loadData();
-  return data.users.find(u => u.linkId === linkId);
+    const db = await connect();
+    return db.collection(usersCol).findOne({ linkId });
 }
 
 async function findUserByUsername(username) {
-  const data = await loadData();
-  return data.users.find(u => u.username === username);
+    const db = await connect();
+    return db.collection(usersCol).findOne({ username });
 }
 
 async function addUser(user) {
-  const data = await loadData();
-  data.users.push(user);
-  await saveData(data);
-  return user;
-}
-
-async function addMessage(linkId, message) {
-  const data = await loadData();
-  if (!data.messages) data.messages = {};
-  if (!Array.isArray(data.messages[linkId])) data.messages[linkId] = [];
-  data.messages[linkId].push(message);
-  await saveData(data);
-  return true;
+    const db = await connect();
+    await db.collection(usersCol).insertOne(user);
 }
 
 async function getMessages(linkId) {
-  const data = await loadData();
-  return Array.isArray(data.messages?.[linkId]) ? data.messages[linkId] : [];
+    const db = await connect();
+    return db.collection(messagesCol).find({ linkId }).sort({ createdAt: -1 }).toArray();
 }
 
-module.exports = { loadData, saveData, findUserByLinkId, findUserByUsername, addUser, addMessage, getMessages };
+async function addMessage(message) {
+    const db = await connect();
+    await db.collection(messagesCol).insertOne(message);
+}
+
+async function deleteMessage(linkId, messageId) {
+    const db = await connect();
+    const result = await db.collection(messagesCol).deleteOne({ linkId, messageId });
+    return result.deletedCount === 1;
+}
+
+module.exports = {
+  findUserByLinkId,
+  findUserByUsername,
+  addUser,
+  getMessages,
+  addMessage,
+  deleteMessage
+};
